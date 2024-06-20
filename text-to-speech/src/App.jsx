e95861af39d1424efcf5ec3d8fcee735dc7d31a2
lucius Jackson <luciusjackson5@gmail.com>
2024-06-20 16:51:13 +0000

    another change

--- text-to-speech/src/App.jsx
+++ text-to-speech/src/App.jsx
@@ -1,33 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import speak from "./scripts/speechScript.js";
+import {checkTextTospeechApiInTheBrowser} from "./scripts/speechScript.js";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+
+window.addEventListener("load",checkTextTospeechApiInTheBrowser)
+    let cols = window.innerWidth-100;
+    return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <textarea name="text" id="" cols={cols} rows={3} className="text"></textarea>
+        <button onClick={speak}>speak</button>
     </>
   )
 }
---
+++ text-to-speech/src/scripts/speechScript.js
@@ -0,0 +1,13 @@
+export function checkTextTospeechApiInTheBrowser(){
+    if("speechSynthesis"in window){
+        alert("your browser supports text to speech functionality")
+    }
+    else{
+        alert(' your browser does not support text to speech functionality')
+    }
+}
+export default function speak(){
+    const text=document.querySelector(".text").value
+    const utterance =new SpeechSynthesisUtterance(text)
+    speechSynthesis.speak(utterance)
+}
